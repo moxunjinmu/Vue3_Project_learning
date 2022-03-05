ebd8bc472cee60939a072d08ba567a5d5b19663a
wuwenjia <1786471868@qq.com>
2022-03-05 23:05:52 +0800

    <perf>:(setup的两个注意点)

---
+++ 06_setup的两个注意点/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Demo @hello="showHello" msg="你好啊" school="school">
+    <span>wwj</span>
+    <template v-slot:wwj>
+      <div>
+        有名字
+      </div>
+    </template>
+  </Demo>
+</template>
+
+<script>
+import Demo from './components/Demo.vue'
+import {h, ref, reactive} from 'vue'
+
+export default {
+  name: 'App',
+  components: {Demo},
+  setup() {
+    function showHello(params) {
+      alert(`你好，我收到了hello事件，参数为${params}`)
+    }
+
+    return {
+      showHello
+    }
+  }
+}
+</script>
---
+++ 06_setup的两个注意点/components/Demo.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>
+    demo组件
+  </h1>
+  <h3>type:{{job.type}}</h3>
+  <h3>sex:{{job.salary}}</h3>
+  <button @click="test">触发demo的hello组件</button>
+</template>
+
+<script>
+import {h, ref, reactive} from 'vue'
+
+export default {
+  name: 'Demo',
+  beforeCreate() {
+    console.log("beforeCreate-start");
+  },
+  props: ['msg', 'school', 'a'],
+  emits: ['hello'],
+  //setup里的props需要props接收
+  setup(props, context) {
+    // console.log("setup-start", props);
+    console.log("context", context);
+    console.log("context.attrs", context.attrs);// 相当于vue2的$attrs
+    console.log("context.emit", context.emit);// 
+    console.log("context.slots", context.slots); // 插槽
+    function test(params) {
+      context.emit("hello", 666)
+    }
+    let job = reactive({
+      salary: "30k",
+      type: "qianduan"
+    })
+
+    return {
+      job,
+      test
+    }
+  }
+}
+</script>
