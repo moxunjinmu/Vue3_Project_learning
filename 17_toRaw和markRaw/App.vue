2c8baf60c33026cf12d4ac0991b9aafd558f77d4
moxunjingmu <1786471868@qq.com>
2022-03-11 00:33:59 +0800

    feat：toRaw和markRaw

---
+++ 17_toRaw和markRaw/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <Demo v-if="isShowDemo"/>
+  <br/>
+  <button @click="isShowDemo = !isShowDemo">切换隐藏demo组件</button>
+  <hr>
+</template>
+
+<script>
+import Demo from './components/Demo.vue'
+import {h, ref, reactive} from 'vue'
+
+export default {
+  name: 'App',
+  components: {Demo},
+  setup(){
+    let isShowDemo = ref(false);
+
+    return {
+      isShowDemo
+    }
+  }
+}
+</script>
