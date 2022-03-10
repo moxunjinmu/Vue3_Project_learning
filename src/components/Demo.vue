3c9ac921d3e85dfbe98fa40422813cc330ca34b4
moxunjingmu <1786471868@qq.com>
2022-03-10 23:49:13 +0800

    test： vue_issue

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,6 +1,6 @@
 <template>
-  <h4>当前的x的值是：{{x.y}}</h4>
-  <button @click="x.y++">点我x++</button>
+  <!-- <h4>当前求和为：{{sum}}</h4>
+  <button @click="sum++">点我x++</button>
   <div>{{person}}</div>
   <h2>姓名:{{ person.name }}</h2>
   <h2>年龄:{{ person.age }}</h2>
@@ -8,21 +8,17 @@
   <button @click="person.name += '$'">修改姓名</button>
   <button @click="person.age += 1">修改年龄</button>
   <button @click="person.job.job1.salary += 1">涨薪</button>
-  <button @click="job.job1.salary += 1">涨薪++</button>
+  <button @click="job.job1.salary += 1">涨薪++</button> -->
 </template>
 
 <script>
-import {  toRefs, shallowReactive, shallowRef } from "vue";
+import {  toRefs, readonly, reactive, shallowReadonly } from "vue";
 
 export default {
   name: "Demo",
   setup() {
-    //* shallowReactive: 只处理最外层属性的响应式 
-    // 只需要监听浅层数据
-    //* shallowRef: 只处理简单类型数据 不进行对象的响应式处理
-    // 后续不会修改对象属性，使用新的对象替换
-
-    let person = shallowReactive({
+    let sum = ref(0)
+    let person = reactive({
       name: "张三",
       age: 18,
       job: {
@@ -31,18 +27,15 @@ export default {
         }
       }
     });
-    
 
-    // let x = shallowRef(0);
-    let x = shallowRef({
-      y: 0
-    })
+    // person内的所有属性数据不允许修改
+    // person = readonly(person)
+    // person = shallowReadonly(person)
 
     // 返回一个对象
     return {
-      x,
-      person,
-      ...toRefs(person)
+      // sum,
+      // ...toRefs(person)
     };
   }
 };
