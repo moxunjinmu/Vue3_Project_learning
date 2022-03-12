e48fc7caf92b88b20ea16c150aab78fe0b4aecc1
moxunjingmu <1786471868@qq.com>
2022-03-12 21:43:14 +0800

    feat：provide与inject

---
+++ 19_provide与inject/components/Son.vue
@@ -0,0 +1,24 @@
+<template lang="">
+  <div class="son">
+    <h3>Son=> name:{{name}}: {{price}}</h3>
+  </div>
+</template>
+<script>
+import { inject, toRefs } from 'vue'
+ export default {
+   name: 'Son',
+   setup(){
+    let car = inject('car') // 拿到app组件的数据
+
+    return {
+      ...toRefs(car)
+    }
+   }
+ }
+</script>
+<style>
+ .son{
+   background-color: #d97c32;
+   padding: 10px;
+ }
+</style>
