286f065ed8992a86123f619c2fe5378c3793f1c6
moxunjingmu <1786471868@qq.com>
2022-03-12 22:30:17 +0800

    feat：teleport组件

---
+++ src/components/Child.vue
@@ -0,0 +1,19 @@
+<template lang="">
+  <div class="Child">
+    <h3>Child</h3>
+    <Son/>
+  </div>
+</template>
+<script>
+import Son from "./Son.vue"
+ export default {
+   name: 'Child',
+   components: {Son}
+ }
+</script>
+<style>
+  .Child{
+    background-color: #8ae234;
+    padding: 10px;
+  }
+</style>
---
+++ src/components/Son.vue
@@ -0,0 +1,22 @@
+<template lang="">
+  <div class="son">
+    
+    <Dialog/>
+  </div>
+</template>
+<script>
+
+import Dialog from './Dialog.vue'
+ export default {
+   name: 'Son',
+   components: {Dialog},
+   setup(){
+   }
+ }
+</script>
+<style>
+ .son{
+   background-color: #d97c32;
+   padding: 10px;
+ }
+</style>
